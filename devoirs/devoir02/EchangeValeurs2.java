d6687745cee1d17701b86a86041783b09f2a5546
Hadi <hadi.harb@studentfr.ch>
2024-09-15 14:43:51 +0200

    fin exe.devoir02

--- devoirs/devoir02/EchangeValeurs2.java
+++ devoirs/devoir02/EchangeValeurs2.java
@@ -4,16 +4,37 @@ import java.util.Random;
 
 public class EchangeValeurs2 {
     public static void main(String args[]) {
-        final int capacité_Réservoire_A = 5;
-        final int capacité_Réservoire_B = 3; // final = constante
-        int reservoirA;
-        int reservoirb;
+        final int capacité_Réservoire_A = 3;
+        final int capacité_Réservoire_B = 5; // final = constante
+        int reservoirA = 0;
+        int reservoirb = 0; // 0 = vide iniatialement
         int remplissage;
         Random rand = new Random(); // code pour avoir une valeur aléatoire
-        remplissage = rand.nextInt(capacité_Réservoire_A + 1); // it will generate a number between 0 and 5 need to add
+        remplissage = rand.nextInt(capacité_Réservoire_B + 1); // it will generate a number between 0 and 5 need to add
                                                                // + 1 to
                                                                // have 5
-        System.out.println(remplissage);
-        // test
+
+        if (remplissage >= 2) {
+            System.out.println("il y a " + remplissage + " litres a remplir");
+        } else {
+            System.out.println("il y a " + remplissage + " litre a remplir");
+        }
+        while (reservoirA + reservoirb < remplissage) {
+
+            if (reservoirA < capacité_Réservoire_A) {
+                System.out.println("Remplisage du réservoir A");
+
+                reservoirA = reservoirA + 1; // reservoirA++ / pour réduire le + devients -
+
+            } else {
+                System.out.println("Remplisage du réservoir B");
+                reservoirb++;
+            }
+
+            System.out.println("le réservoir A: " + reservoirA + ", le réservoir B: " + reservoirb);
+        }
+
+            System.out.println("remplissage terminé!");
     }
+
 }
