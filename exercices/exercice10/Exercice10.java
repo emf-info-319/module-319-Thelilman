9b65c72fa87c1a59cd1f8ba04d5cefcf1e1da41d
Hadi Harb <Hadi.harb@studentfr.ch>
2024-10-31 13:24:21 +0100

    introMethode

---
+++ exercices/exercice10/Exercice10.java
@@ -0,0 +1,10 @@
+public class Exercice10 {
+    public static void main(String[] args) {
+        bonjour();
+        
+    }
+
+    public static void bonjour(){
+        System.out.println("Bonjour");
+    }
+}
