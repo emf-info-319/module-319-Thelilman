c84442696b6e612e00b9b378ef1714c7501aaf74
Hadi <hadi.harb@studentfr.ch>
2025-06-03 11:09:40 +0200

    jkbwdwkjed

---
+++ revisions/remed/E2.java
@@ -0,0 +1,21 @@
+package revisions.remed;
+
+public class E2 {
+public final static INT TAILLE_PLATEAU = 20;
+public final static String CASE_VIDE = " ";
+public final static String CASE_REJOUER = "R";
+public final static String CASE_RECULER = "B";
+public final static String CASE_AVANCER = "A";
+public final static String CASE_DEPART = "D";
+public final static String CASE_FIN = "F";
+public static void main(String[] args) {
+    System.out.println("Hello, World!");
+Scanner scanner = new Scanner(System.in);
+  plateau = initialiserPlateau();
+  positionJoueur = 0;
+    boolean jeuTermine = false;
+    nombreDeLance = 0;
+    System.out.println("Bienvenue dans le jeu de l'oie !");
+    
+}
+}
